20e6cc4eb903a523a4c5644bd9c045e8be90206b
Szymon Kowaliński <szymon@kowalinski.dev>
2024-05-19 19:37:39 +0200

    feat: adjust django template

--- docker/backend/server-entrypoint.sh
+++ docker/backend/server-entrypoint.sh
@@ -17,7 +17,8 @@ python manage.py collectstatic --noinput
 
 # python manage.py createsuperuser --noinput
 
-gunicorn backend.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
+# for prod
+#gunicorn backend.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
 
 # for debug
-#python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
