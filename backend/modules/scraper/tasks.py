d6cb8a5ea2f48a0d59cddb32b06d38672ad53e8b
Szymon Kowaliński <szymon@kowalinski.dev>
2024-06-18 19:08:25 +0200

    feat: fetch_apartments_task

--- backend/modules/scraper/tasks.py
+++ backend/modules/scraper/tasks.py
@@ -1,29 +1,25 @@
 from celery.utils.log import get_task_logger
-
 from modules.apartments.models import Apartment
 from modules.core.utils import celery_task
+from modules.scraper.services.custom_logger import CustomLogger
+from modules.scraper.services.scraper import ScraperService
 
-logger = get_task_logger(__name__)
-
-
-@celery_task
-def hello(self):
-    logger.info("Hello world")
-    logger.info(self)
-    return {"nice": "hello world"}
+celery_logger = get_task_logger(__name__)
 
 
 @celery_task
-def fetch_apartment_details(self, apartment: Apartment):
+def fetch_apartment_details_task(self, apartment: Apartment):
     # create details or update if exists
     ...
 
-@celery_task
-def fetch_apartments_from_list(self, url: str):
-    ...
 
 @celery_task
-def follow_apartments_by_url(self, url: str):
-    # lece po listview
-    # dodaje taski sprawdzania details
-    ...
+def fetch_apartments_task(_, url: str):
+    logger = CustomLogger(celery_logger)
+    next_page_url = ScraperService(logger).fetch_apartments(url)
+    if next_page_url is not None:
+        fetch_apartments_task.apply_async(
+            args=[str(next_page_url)],
+            countdown=3,  # run next in 3 sec
+        )
+    return logger.get_result_dict()
