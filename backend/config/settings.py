94c0b0f301ab87cf41dca9ee794aa4bd546e31f4
Szymon Kowaliński <szymon@kowalinski.dev>
2024-06-18 02:55:32 +0200

    refactor: remove celery result

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_extensions",
     "django_celery_beat",
-    "django_celery_results",
     # Modules
     "modules.apartments",
     "modules.emails",
