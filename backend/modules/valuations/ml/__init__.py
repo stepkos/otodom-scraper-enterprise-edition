59c7f5e7ab2f6418e1c85a391cabb20500586695
stepkos <stepkos@wp.pl>
2024-06-19 04:07:54 +0200

    feat: Add scalers to data

--- backend/modules/valuations/ml/__init__.py
+++ backend/modules/valuations/ml/__init__.py
@@ -1,11 +1,19 @@
+__all__ = ['mlp', 'scaler_in', 'scaler_out']
+
 from pathlib import Path
 
 import torch
+import joblib
 
 from modules.valuations.ml.net import MLP
 
 model_path = Path(__file__).parent / 'models' / "estimate-wro-aparts-mlp.pth"
+scaler_x_path = Path(__file__).parent / 'scalers' / "estimate-wro-aparts-mlp-xscaler.pkl"
+scaler_y_path = Path(__file__).parent / 'scalers' / "estimate-wro-aparts-mlp-yscaler.pkl"
 
-trained_mlp = MLP()
-trained_mlp.load_state_dict(torch.load(str(model_path)))
-trained_mlp.eval()
+mlp = MLP()
+mlp.load_state_dict(torch.load(str(model_path)))
+mlp.eval()
+
+scaler_in = joblib.load(str(scaler_x_path))
+scaler_out = joblib.load(str(scaler_y_path))
--- backend/modules/valuations/ml/predictions.py
+++ backend/modules/valuations/ml/predictions.py
@@ -1,10 +1,20 @@
 import torch
 
-from modules.valuations.ml import trained_mlp
+from modules.valuations.ml import mlp, scaler_in, scaler_out
 
 
 def predict_raw(input_data: list[float]) -> float:
     assert len(input_data) == 19, "Input data must have 19 elements"
 
     with torch.no_grad():
-        return trained_mlp(torch.tensor(input_data, dtype=torch.float32)).item()
+        return mlp(torch.tensor(input_data, dtype=torch.float32)).item()
+
+
+def predict_with_scalers(input_data: list[float]) -> float:
+    assert len(input_data) == 19, "Input data must have 19 elements"
+
+    with torch.no_grad():
+        in_scaled = scaler_in.transform([input_data])
+        in_tensor = torch.tensor(in_scaled, dtype=torch.float32)
+        out_numpy = mlp(in_tensor).detach().numpy()
+        return scaler_out.inverse_transform(out_numpy).item()
